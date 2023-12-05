eb7bae9d823d86c34f7c5b3f6eca640b3a0d9890
leandro-santiago <69202675+leandros1793@users.noreply.github.com>
2023-12-05 03:44:59 -0300

    agregue el codigo

---
+++ src/components/Error.jsx
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled"
+const Texto = styled.div `
+background-color: #B7322c;
+color: #FFF;
+padding: 15px;
+font-size: 22px;
+text-transform: uppercase;
+font-family: 'Lato', sans-serif;
+font-weight: 700;
+text-align: center;
+
+
+
+
+
+`
+const Error = ({children}) => {
+  return (
+    <div>
+      {Children}
+    </div>
+  )
+}
+
+export default Error
